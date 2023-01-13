e17449c683d3bd7e0c501712fda234162746b892
mingun choi <ryanchoi100826@gmail.com>
2023-01-13 10:16:07 -0500

    first commit

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if(actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`)
+  } else { 
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  }
+};
+
+const findKey = function(object, callback) {
+  let keysArr = Object.keys(object);
+  for (let i = 0; i < keysArr.length; i++) {
+    if (callback(object[keysArr[i]])) {
+      return keysArr[i];
+    }
+  }
+  return undefined;
+};
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
