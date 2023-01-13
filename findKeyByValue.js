074fcbb3bee36900f0bd9e7079fa3fc384ab1f7b
mingun choi <ryanchoi100826@gmail.com>
2023-01-13 10:16:22 -0500

    modified

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -27,4 +27,3 @@ const findKeyByValue = function (array) {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-
